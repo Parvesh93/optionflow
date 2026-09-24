bfa189c9aa94ff28df15fc5949e91e389fd04345
Parvesh93 <141941315+Parvesh93@users.noreply.github.com>
2026-09-25 01:16:15 +0530

    Add product assignment management

---
+++ app/routes/app.option-sets.$optionSetId.assignments.tsx
@@ -0,0 +1,11 @@
+export {
+  productAssignmentsLoader as loader,
+} from "~/features/assignments/loader.server";
+
+export {
+  productAssignmentsAction as action,
+} from "~/features/assignments/action.server";
+
+export {
+  default,
+} from "~/features/assignments/pages/ProductAssignmentsPage";
