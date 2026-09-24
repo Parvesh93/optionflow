6a0a4de3d729b99ba8ed1c3a5d53b812d2489b57
Parvesh93 <141941315+Parvesh93@users.noreply.github.com>
2026-09-24 23:29:38 +0530

    Add option builder route

---
+++ app/routes/app.option-sets.$optionSetId.builder.tsx
@@ -0,0 +1,11 @@
+export {
+  optionBuilderLoader as loader,
+} from "~/features/options/loader.server";
+
+export {
+  optionBuilderAction as action,
+} from "~/features/options/action.server";
+
+export {
+  default,
+} from "~/features/options/pages/OptionBuilderPage";
